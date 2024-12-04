85920fa0b05513a01fcd67b9473576de03a28ed9
biubiubiupiu21 <197yjy@gmail.com>
2024-12-04 16:23:01 -0500

    set up the firebase

--- Pit Hub/Pit_HubApp.swift
+++ Pit Hub/Pit_HubApp.swift
@@ -6,14 +6,39 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    checkFirebaseConnection()
+    return true
+  }
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+      checkFirebaseConnection()
+    }
+
+    private func checkFirebaseConnection() {
+        if FirebaseApp.app() == nil {
+            // Handle the error, for instance, show an alert to the user
+            print("Firebase configuration failed.")
+        } else {
+            // Optionally, you can perform additional checks or log success
+            print("Firebase is configured successfully!")
+        }
+    }
+}
 
 @main
 struct Pit_HubApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     let persistenceController = PersistenceController.shared
 
     var body: some Scene {
         WindowGroup {
-            SplashView()
+            BottomNavBar()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
