4474ca0f47b8d1dbe5dfce7226c9a4093c2fbaf8
biubiubiupiu21 <197yjy@gmail.com>
2025-03-27 06:26:56 -0400

    Create HomeCalendarView.swift

---
+++ Pit Hub/View/Home/View/HomeCalendarView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeCalendarView.swift
+//  Pit Hub
+//
+//  Created by Junyu Yao on 3/27/25.
+//
+
+import SwiftUI
+
+struct HomeCalendarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeCalendarView()
+}
